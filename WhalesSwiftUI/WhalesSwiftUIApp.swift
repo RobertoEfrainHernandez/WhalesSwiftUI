864c130f8697acfd093e98891e13f9dead1cba53
Roberto Efrain Hernandez <bh21346n@pace.edu>
2021-02-28 11:00:23 -0800

    Initial Commit

---
+++ WhalesSwiftUI/WhalesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhalesSwiftUIApp.swift
+//  WhalesSwiftUI
+//
+//  Created by Roberto Hernandez on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct WhalesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
